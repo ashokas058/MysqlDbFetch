3f796a1c81dad7f3de4819f5f398a8d6321b2447
Ashok AS <ashokas058@gmail.com>
2021-05-08 17:52:43 -0700

    Update AsyncSend.java

--- app/src/main/java/com/ashokas/userprofileui/ASYNC/AsyncSend.java
+++ app/src/main/java/com/ashokas/userprofileui/ASYNC/AsyncSend.java
@@ -15,22 +15,18 @@ import java.net.URLEncoder;
 public class AsyncSend extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... strings) {
-        String callDate = strings[0];
-        String callTime = strings[1];
-        String sysDate = strings[2];
-        String callGiotag = strings[3];
-        String callNumber = strings[4];
-        String callid = strings[5];
+        String strName = strings[0];
+        String strEmail = strings[1];
+        String strCollege = strings[2];
+       
 
         try {
+            //this code was re-edited for matching DataModel-(Person) without IDE.
 
-            String data = URLEncoder.encode("C_MOBILE", "UTF-8") + "=" + URLEncoder.encode(callNumber, "UTF-8") + "&"
-                    + URLEncoder.encode("C_DATE", "UTF-8") + "=" + URLEncoder.encode(callDate, "UTF-8") + "&"
-                    + URLEncoder.encode("C_TIME", "UTF-8") + "=" + URLEncoder.encode(callTime, "UTF-8") + "&" +
-                    URLEncoder.encode("C_GIOTAG", "UTF-8") + "=" + URLEncoder.encode(callGiotag, "UTF-8") + "&" +
-                    URLEncoder.encode("SYSDATE", "UTF-8") + "=" + URLEncoder.encode(sysDate, "UTF-8") + "&" +
-                    URLEncoder.encode("C_LOGID", "UTF-8") + "=" + URLEncoder.encode(callid, "UTF-8");
-            URL url = new URL("http://k.pythonanywhere.com/test-api");
+            String data = URLEncoder.encode("C_NAME", "UTF-8") + "=" + URLEncoder.encode(strName, "UTF-8") + "&"
+                    + URLEncoder.encode("C_EMAIL", "UTF-8") + "=" + URLEncoder.encode(strEmail, "UTF-8") + "&"
+                    + URLEncoder.encode("C_COLLEGE", "UTF-8") + "=" + URLEncoder.encode(strCollege, "UTF-8");
+            URL url = new URL("http://k.pythonanywhere.com/phpInsert.php");
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
             conn.setDoInput(true);
