9800c46166ba8c2d232860e8639080c37b42ca84
Ashok AS <ashokas058@gmail.com>
2021-05-08 17:39:58 -0700

    Update PhpInsert.php

--- Php/PhpInsert.php
+++ Php/PhpInsert.php
@@ -1,17 +1,17 @@
 <?php
 
 
-$C_LOGID="dfff";
-$C_DATE="dddd";
-$C_TIME="dxs";
-$C_MOBILE="ddd";
-$C_GIOTAG="ddffs";
-$SYSDATE="ddsfsd";
+$C_LOGID=urldecode($_POST['C_LOGID']);
+$C_DATE=urldecode($_POST['C_DATE']);
+$C_TIME=urldecode($_POST['C_TIME']);
+$C_MOBILE=urldecode($_POST['C_MOBILE']);
+$C_GIOTAG=urldecode($_POST['C_GIOTAG']);
+$SYSDATE=urldecode($_POST['SYSDATE']);
 
 
 $host = 'localhost';
-$user = 'id1247ddf';
-$pass_db = 'lucifeaa';
+$user = 'id11297169_shome';
+$pass_db = 'U6k#~U%Cs+}\szG)';
 $conn=mysqli_connect($host, $user, $pass_db,$user);
 
 $dologin = "select id,pass from user where id = $id and pass = $pass ";
